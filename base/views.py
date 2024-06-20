2ac55d92e23d44a3a3edacb6d710227ffb23473d
Sathish15112002 <satkav15112002@gmail.com>
2024-06-20 06:01:25 +0530

    Add files via upload

---
+++ base/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+import pickle
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+global scaler
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+def HomePage(request):
+    return render (request,'home.html')
+
+def SignupPage(request):
+    if request.method=='POST':
+        uname=request.POST.get('username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+
+        if pass1!=pass2:
+            return HttpResponse("Your password and confrom password are not Same!!")
+        else:
+
+            my_user=User.objects.create_user(uname,email,pass1)
+            my_user.save()
+            return redirect('login')
+        
+
+
+
+    return render (request,'signup.html')
+
+def LoginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('pass')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('index')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+
+    return render (request,'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('home')
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
+
+
+def getPredictions(x):
+    x=int(x[5:])
+    model = pickle.load(open('naive_bayes.pkl', 'rb'))
+    prediction = model.predict(np.array([[x]]))
+    return (prediction)
+
+def result(request):
+    x = str(request.GET['DOID'])
+    result = getPredictions(x)
+    return render(request, 'result.html', {'result': result[0]})
+    #return render(request, 'result.html', {'result': result})
+
